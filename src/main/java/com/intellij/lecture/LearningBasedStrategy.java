3ed4c61c5a79dc807f7f0875be552c96000e47fb
MF57 <p-bochenek@outlook.com>
2016-03-17 15:32:18 +0100

    pom xml for testing

--- src/main/java/com/intellij/lecture/LearningBasedStrategy.java
+++ src/main/java/com/intellij/lecture/LearningBasedStrategy.java
@@ -5,13 +5,6 @@ package com.intellij.lecture;
  */
 public class LearningBasedStrategy implements WritingExamStrategy {
     public double writeExam(Student student, Exam exam) {
-
-        int studentResources = student.getIq() + student.getTime();
-        int examChallenge = exam.getDifficulty() + exam.getTimeConsumption();
-        if(examChallenge == 0) {
-            return 100;
-        }
-        int result = (studentResources * 100) / examChallenge;
-        return result > 100 ? result : 100;
+        return (student.getIq()+student.getTime())*100/(exam.getDifficulty() + exam.getTimeConsumption());
     }
 }
--- src/main/java/com/intellij/lecture/WritingExamStrategy.java
+++ src/main/java/com/intellij/lecture/WritingExamStrategy.java
@@ -1,7 +1,6 @@
 package com.intellij.lecture;
 
-import com.intellij.lecture.Exam;
-import com.intellij.lecture.Student;
+
 
 /**
  * Created by mf57 on 13.03.2016.
